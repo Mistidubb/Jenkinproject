7724a7af31b3fe5ada97a032a3432d7cddbe1dfe
Mistdubb <mistidubb@gmail.com>
2021-09-16 18:48:08 +0100

    update file

--- src/main/java/com/misti/FirstExample.java
+++ src/main/java/com/misti/FirstExample.java
@@ -2,7 +2,7 @@ package com.misti;
 
 public class FirstExample {
 	public static void main(String[] args) {
-		System.out.println("Welcome java");
+		System.out.println("Welcome to my First Application ");
 	}
 
 }
